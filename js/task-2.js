2f7162c46e706dd8f97ad7edc8666e051c67c6f2
Yurii <yerikstar@gmail.com>
2025-06-18 16:57:45 +0300

    add task-2

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+function calcAverageCalories(days) {
+    let sum = 0;
+    for (const days.calories of )
+}
+
+
+
+
+
+// function getAllPropValues(propName) {
+//     const products = [
+//       { name: "Radar", price: 1300, quantity: 4 },
+//       { name: "Scanner", price: 2700, quantity: 3 },
+//       { name: "Droid", price: 400, quantity: 7 },
+//       { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+//     const result = [];
+//      for (const product of products) {
+//        if (propName in product) {
+//          result.push(product[propName]);
+//        }
+//      }
+//     return result;
+//   }
