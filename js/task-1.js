690eba27e7449fc6c5e325f019e7972566f879d7
Yurii <yerikstar@gmail.com>
2025-06-18 16:39:48 +0300

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,10 @@
 function isEnoughCapacity(products, containerSize) {
-    
+    let totalProducts = 0;
+    const values = Object.values(products);
+    for (const value of values) {
+        totalProducts += value;
+    }
+    return totalProducts <= containerSize;    
 }
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
